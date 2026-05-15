9a286786c1ae583f7a8b442921ba63331f2ef2e2
Nithya Shaji <nithyashaji2006@gmail.com>
2026-05-15 14:18:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
